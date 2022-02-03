118a2ee594be78210b46082fed7f2a2964c3a46a
yitzy32 <yitzybratt32@gmail.com>
2022-02-03 10:54:01 -0500

    conditionals

--- src/App.java
+++ src/App.java
@@ -2,24 +2,118 @@ class App {
   public static void main(String[] args) {
     // 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
+    // int number = 10;
+
+    // if (number == 10) {
+    //   System.out.println(0);
+    // } else {
+    //   System.out.println(-1);
+    // }
+
     // 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+    // int number = 9;
+
+    // if (number < 10) {
+    //   System.out.println(-1);
+    // } else if (number > 10) {
+    //   System.out.println(1);
+    // } else if (number == 10) {
+    //   System.out.println(0);
+    // }
 
     // 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
+      // int numberOne = 8;
+      // int numberTwo = 5;
+
+      // if (numberOne < 10 && numberTwo < 10) {
+      //   System.out.println(1);
+      // } else {
+      //   System.out.println(0);
+      // }
+
     // 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+      // int number = 9;
+      // if (number > 9000) {
+      //   System.out.println(1);
+      // } else {
+      //   System.out.println(-1);
+      // }
 
     // 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
 
+        // int number = 9; 
+        // if (number <10) {
+        //   System.out.println(9);
+        // } else if (number <20) {
+        //   System.out.println(19);
+        // } else if (number <30) {
+        //   System.out.println(29);
+        // } else {
+        //   System.out.println(-1);
+        // }
+
     // 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+      // int numberOne = 9;
+      // int numberTwo = 8;
+
+      // if (numberOne > 10 || numberTwo > 10) {
+      //   System.out.println(100);
+      // } else {
+      //   System.out.println(-100);
+      // }
 
     // 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
+      // int number = 9;
+
+      // if (number < 0) {
+      //   System.out.println(1776);
+      // } else {
+      //   System.out.println(1979);
+      // }
+
     // 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+      // int number = 9;
+      
+      // if (number == 100) {
+      //   System.out.println(100);
+      // } else if (number == 99) {
+      //   System.out.println(99);
+      // } else {
+      //   System.out.println(0);
+      // }
 
     // 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
+      // int numberOne = -9;
+      // int numberTwo = 9;
+
+      // if (numberOne < 0 && numberTwo > 0) {
+      //   System.out.println(1);
+      // } else {
+      //   System.out.println(0);
+      // }
+
     // 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
+    // int number = 9;
+
+    // if (number > 80) {
+    //   System.out.println(5);
+    // } else if (number > 60) {
+    //   System.out.println(4);
+    // } else if (number > 40) {
+    //   System.out.println(3);
+    // } else if (number > 20) {
+    //   System.out.println(2);
+    // } else {
+    //   System.out.println(1);
+    // }
 
     // SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
 
