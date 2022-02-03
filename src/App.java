c035f20bc11934371c9d7baf4464caa8d9b2a9f0
yitzy32 <yitzybratt32@gmail.com>
2022-02-03 11:33:31 -0500

    strings concatination and interpolation

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class App {
   public static void main(String[] args) {
     // 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
@@ -120,29 +122,95 @@ class App {
 
     // 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-      String firstName = "bob";
-      String lastName = "the builder";
+      // String firstName = "Bob";
+      // String lastName = "The Builder";
 
-      System.out.println(firstName + " " + lastName);
+      // System.out.println(firstName + " " + lastName);
 
     // 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
+      // String firstName = "Bob";
+      // String lastName = "The Builder";
+      // String message = String.format("%s %s", firstName,lastName);
+
+      // System.out.println(message);
+
     // 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+      // System.out.println("input a word please");
+      // Scanner keyboard = new Scanner(System.in);
+      // String word = keyboard.next();
+
+      // if (word.equals("marco")) {
+      //   System.out.println("polo");
+      // }
+      // keyboard.close();
 
     // 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
+      // String blue = "blue";
+      // String red = "red";
+      // String green = "green";
+
+      // System.out.println("the colors are: " + blue + " " + red + " and " + green);
+
     // 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+    // String blue = "blue";
+    // String red = "red";
+    // String green = "green";
+    // String message = String.format("the colors are %s %s and %s", blue,green,red);
+
+    // System.out.println(message);
 
     // 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+    // System.out.println("input a name please");
+    //   Scanner keyboard = new Scanner(System.in);
+    //   String name = keyboard.next();
+
+    //   if (!name.equals("Santa")) {
+    //     System.out.println("You're not Santa.");
+    //   }
+    //   keyboard.close();;
+
     // 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+      // String title = "Java for Dummies";
+      // String author = "Barry Burd";
+
+      // System.out.println("The author of " + title + " is " + author);
 
     // 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+    // String title = "Java for Dummies";
+    // String author = "Barry Burd";
+    // String message = String.format("The author of %s is %s", title,author);
+
+    // System.out.println(message);
+
     // 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+      // System.out.println("Please enter a password");
+
+      // Scanner keyboard = new Scanner(System.in);
+      // String password = keyboard.next();
+
+      // if (password.equals("Joshua")) {
+      //   System.out.println("Shall we play a game?");
+      // } else {
+      //   System.out.println("Access denied");
+      // }
+
+      // keyboard.close();
 
     // 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
+      // String brooklyn = "Brooklyn";
+      // String queens = "queens";
+      // String bronx = "bronx";
+
+      // System.out.println(brooklyn + ", " + queens + ", and " + bronx + " are all cities");
 
     // SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
 
